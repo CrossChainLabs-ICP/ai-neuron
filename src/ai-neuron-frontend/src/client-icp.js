2f5edc76180f74ed745ee013e03bdd111dadf4ae
yellow-cube <93196588+yellow-cube@users.noreply.github.com>
2025-07-31 17:26:15 +0300

    Cleanup

--- src/ai-neuron-frontend/src/client-icp.js
+++ src/ai-neuron-frontend/src/client-icp.js
@@ -16,7 +16,6 @@ export class ClientICP {
     this.agent = agent;
     this.workers = [];
     this.mainActor = createMainActor(CANISTER_ID, {agent});
-    console.log(`ClientICP[${CANISTER_ID}]`);
   }
 
   async init() {
@@ -38,7 +37,6 @@ export class ClientICP {
   }
 
     async get_reports_list(start, size) {
-      console.log('get_full_reports');
       let items = [];
 
       try {
@@ -46,17 +44,10 @@ export class ClientICP {
           await this.init();
         }
 
-        console.log(this.workers);
 
       //determine the correct worker
       const workerOffset = parseInt(start / MAX_ITEMS)
       let currentWorker = null;
-
-      console.log('workerOffset', workerOffset);
-
-      console.log('this.workers', this.workers);
-
-
 
       if (workerOffset >= 0 && workerOffset <= this.workers.length) {
         currentWorker = this.workers[this.workers.length - workerOffset - 1];
@@ -78,7 +69,6 @@ export class ClientICP {
 
   async get_full_reports(start, size) {
     let items = [];
-    console.log('get_full_reports');
 
     try {
       if (this.workers?.length <= 0) {
@@ -89,18 +79,13 @@ export class ClientICP {
       const workerOffset = parseInt(start / MAX_ITEMS)
       let currentWorker = null;
 
-      console.log('workerOffset', workerOffset);
-
-      console.log('this.workers', this.workers);
-
       if (workerOffset >= 0 && workerOffset <= this.workers.length) {
         currentWorker = this.workers[this.workers.length - workerOffset - 1];
       }
 
-      const metadata_reports_list = await currentWorker.get_reports_list(start, size);
-      console.log(metadata_reports_list);
-      if (metadata_reports_list?.length > 0) {
-        items = await currentWorker.get_full_reports(metadata_reports_list);
+      const reports_ids_list = await currentWorker.get_reports_list(start, size);
+      if (reports_ids_list?.length > 0) {
+        items = await currentWorker.get_full_reports(reports_ids_list);
       } else {
         return [];
       }
--- src/ai-neuron-frontend/src/config.js
+++ src/ai-neuron-frontend/src/config.js
@@ -1,3 +1,7 @@
 //Dev
 export const HOST = 'http://127.0.0.1:4943';
 export const CANISTER_ID = 'uxrrr-q7777-77774-qaaaq-cai';
+
+//Prod
+//export const HOST = 'http://127.0.0.1:4943';
+//export const CANISTER_ID = 'uxrrr-q7777-77774-qaaaq-cai';
