e5d43a1cf0eb60024fc7bba56ab18d3252e291d8
yellow-cube <93196588+yellow-cube@users.noreply.github.com>
2025-08-01 13:11:16 +0300

    Update config.js

--- src/ai-neuron-frontend/src/config.js
+++ src/ai-neuron-frontend/src/config.js
@@ -3,5 +3,5 @@ export const HOST = 'http://127.0.0.1:4943';
 export const CANISTER_ID = 'uxrrr-q7777-77774-qaaaq-cai';
 
 //Prod
-//export const HOST = 'http://127.0.0.1:4943';
+//export const HOST = 'https://ic0.app';
 //export const CANISTER_ID = 'uxrrr-q7777-77774-qaaaq-cai';
